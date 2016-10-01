fa3a508541315d0b0a3a065c2ca485782aa6c9f2
pacman <pcameron5@gmail.com>
2016-10-01 09:27:11 -0400

    Created a menu and test mode

--- test.py
+++ test.py
@@ -1,57 +1,78 @@
 import os
 
 
-def controller(response=""):
-    if response is not "":
-        print(response)
+class master_lesson(object):
+    def __init__(self):
+        self.car_models = ['', 'Ford', 'Dodge', 'Chevy', 'None']
+        self.test = False
 
-    car_models = ['Ford', 'Dodge', 'Chevy', 'None']
+    def controller(self, response=""):
+        if response is not "":
+            print(response)
 
-    display(car_models)
+        self.screen_display(self.car_models)
 
-    reply = int(input("Sir, what type of car do you own? > "))
+        try:
+            reply = int(input("Sir, what type of car do you own? > "))
+        except:
+            self.reset()
+            return self.controller("You entered a string not an integer")
 
-    validate = list_checker(car_models, reply)
-    if not validate:
-        reset()
-        return controller("That choice wasn't an option, try again!")
-    else:
-        reset()
-        return "Completed, returned {}".format(car_models[validate])
+        validate = self.list_checker(self.car_models, reply)
 
-    reset()
-    return controller("That's not in the list, try again")
+        if self.test:
+            print(" validate == {}, type{}".format(validate, type(validate)))
 
+        if not validate:
+            self.reset()
+            return self.controller("That choice wasn't an option, try again!")
+        else:
+            self.reset()
+            return "Completed, returned {}".format(self.car_models[validate])
 
-def display(list_car):
-    for x, each in enumerate(list_car):
-        print("{}) {}".format(x, each))
+        self.reset()
+        return controller("That's not in the list, try again")
 
+    @staticmethod
+    def screen_display(list_car):
 
-def reset():
-    os.system('clear')  # on linux / os x
+        for x, each in enumerate(list_car):
+            if x is 0:
+                pass
+            else:
+                print("{}) {}".format(x, each))
 
+        print("9) Exit")
 
-def list_checker(list_x, user_answer):
+    def reset(self):
+        if not self.test:
+            os.system('clear')  # on linux / os x
 
-    for position, each in enumerate(list_x):
-        # print("{}, {}".format(type(position), type(user_answer)))
-        if position == user_answer:
-            return position
+    def list_checker(self, list_x, user_answer):
 
-    return False
+        if user_answer == int(9):
+            exit()
 
+        for position, each in enumerate(list_x):
+            if self.test:
+                print("{}, {}".format(type(position), type(user_answer)))
 
-def checker(x):
-    if x.isalpha():
-        return x
+            if position == user_answer:
+                if self.test:
+                    print("answer passed")
+                return position
 
-    elif int(x):
-        print("I'm a integer")
         return False
 
-    else:
-        return False
+    def checker(x):
+        if x.isalpha():
+            return x
 
+        elif int(x):
+            print("I'm a integer")
+            return False
 
-print(controller())
+        else:
+            return False
+
+print(master_lesson().controller())
