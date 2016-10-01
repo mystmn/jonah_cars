7390bced40448bd94154424715482dd9e898c92c
pacman <pcameron5@gmail.com>
2016-10-01 09:02:24 -0400

    Added the option to preselect from list instead of variable = strings

--- test.py
+++ test.py
@@ -7,23 +7,39 @@ def controller(response=""):
 
     car_models = ['Ford', 'Dodge', 'Chevy', 'None']
 
-    reply = input("Sir, what type of car do you own? {} > ".format(car_models))
+    display(car_models)
 
-    if not checker(reply):
-        os.system('clear')  # on linux / os x
-        return controller("That choice isn't a string, try again!")
+    reply = int(input("Sir, what type of car do you own? > "))
 
-    for x in car_models:
-        if reply == x:
-            print(x)
-            return True
+    validate = list_checker(car_models, reply)
+    if not validate:
+        reset()
+        return controller("That choice wasn't an option, try again!")
+    else:
+        reset()
+        return "Completed, returned {}".format(car_models[validate])
 
     reset()
     return controller("That's not in the list, try again")
 
 
+def display(list_car):
+    for x, each in enumerate(list_car):
+        print("{}) {}".format(x, each))
+
+
 def reset():
     os.system('clear')  # on linux / os x
+
+
+def list_checker(list_x, user_answer):
+
+    for position, each in enumerate(list_x):
+        # print("{}, {}".format(type(position), type(user_answer)))
+        if position == user_answer:
+            return position
+
+    return False
 
 
 def checker(x):
